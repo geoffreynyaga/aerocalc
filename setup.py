6f5571ba3d6b638585a7eca0ad7d9344df3f6e1c
geoffreynyaga <geoffreynyagak@gmail.com>
2020-04-11 13:13:13 +0300

    rename to aerocalc3

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name="aerocalc",
+    name="aerocalc3",
     version="0.10",
-    description="Aeronautical Engineering Calculations",
+    description="Aeronautical and Atmospheric Engineering Calculations",
     long_description="""
-                        AeroCalc is a pure python package that performs various aeronautical
+                        Aero-calculator is a pure python package that performs various aeronautical
                         engineering calculations.  Currently it provides airspeed conversions,
                         standard atmosphere calculations, static source error correction calculations
                         and unit conversions.
                     """,
     author="Geoffrey Nyaga",
     author_email="geoffrey@geoffreynyaga.com",
-    url="https://geoffreynyaga.com/aerocalc/",
+    url="https://geoffreynyaga.com/aerocalc3/",
     download_url="https://github.com/geoffreynyaga/aerocalc/archive/v_01.tar.gz",
-    packages=["aerocalc"],
+    packages=["aerocalc3"],
     license="BSD",
     classifiers=[
         "Development Status :: 4 - Beta",
